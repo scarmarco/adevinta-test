0516330fef58b3f00986192a096d676706814bb2
Marco Scarciofolo <scar_marco@hotmail.es>
2019-12-03 22:17:37 -0300

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const accordion = {
     const accordionTriggers = document.querySelectorAll("dt");
     accordionTriggers.forEach(elm => {
       elm.addEventListener("click", this.handleClick);
-      elm.setAttribute("data-before", "+");
     });
   },
   handleClick: function(evt) {
@@ -11,12 +10,6 @@ const accordion = {
     const content = trigger.nextElementSibling;
     trigger.classList.toggle("is-active");
     content.classList.toggle("is-open");
-
-    if (trigger.classList.contains("is-active")) {
-      trigger.setAttribute("data-before", "-");
-    } else {
-      trigger.setAttribute("data-before", "+");
-    }
   }
 };
 
